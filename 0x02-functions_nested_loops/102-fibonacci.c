d8049d3c414616a9ea7beca19deede9a96d1c147
Oladunni-Eludire <oladunnieludire@gmail.com>
2023-06-21 14:57:30 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0, count < 50; count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
-		printf("lu", sum);
+		printf("%lu", sum);
 
 		fib1 = fib2;
-		fibb2 = sum;
+		fib2 = sum;
 
 		if (count == 49)
-			prinf("\m");
+			printf("\n");
 		else
-			printf(", ";
+			printf(", ");
 	}
 
 	return (0);
