d91af60b6ea871ca4fad373d4bfc0e4515d12780
Oladunni-Eludire <oladunnieludire@gmail.com>
2023-06-29 22:37:26 -0700

    update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string 1
+ * @src: string 2
+ * @n: 
+ *
+ * Return: 1 is for digits, 0 is for others.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
