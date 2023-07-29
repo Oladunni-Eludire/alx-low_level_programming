ce2ceb90a8c68568570302afbcb3adf19bcce588
Oladunni-Eludire <oladunnieludire@gmail.com>
2023-07-29 00:58:35 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+void __attribute__ ((constructor)) first(void);
+
 /**
  * first - prints lines before the new line of a function
  */
+
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
