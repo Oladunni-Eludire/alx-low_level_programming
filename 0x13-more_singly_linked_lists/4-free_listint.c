378457f344ee0b04c1cd363bc6e26e7bc22ee686
Oladunni-Eludire <oladunnieludire@gmail.com>
2023-08-01 20:36:14 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head);
+		head = t;
+	}
+}
